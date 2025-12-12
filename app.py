bd76ae8789af644901873daea1b1b36ab5a2e941
shehab358 <shehabyusuf12@gmail.com>
2025-12-12 22:40:35 +0200

    s

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-import tensorflow.lite as tflite
+import tflite_runtime.interpreter as tflite
 
 st.title("Customer Frequency Prediction App")
 st.write("Predict whether a customer is frequent (Frequency > 1) based on Recency, Monetary, and AvgQuantity.")
@@ -9,7 +9,6 @@ st.write("Predict whether a customer is frequent (Frequency > 1) based on Recenc
 interpreter = tflite.Interpreter(model_path="model.tflite")
 interpreter.allocate_tensors()
 
-# Get input & output indices
 input_index = interpreter.get_input_details()[0]['index']
 output_index = interpreter.get_output_details()[0]['index']
 
@@ -20,15 +19,12 @@ monetary = st.number_input("Monetary (total spent)", min_value=0.0)
 avg_quantity = st.number_input("Average Quantity per order", min_value=0.0)
 
 if st.button("Predict"):
-    # Prepare input array
     input_array = np.array([[recency, monetary, avg_quantity]], dtype=np.float32)
 
-    # Run the TFLite model
     interpreter.set_tensor(input_index, input_array)
     interpreter.invoke()
     probability = interpreter.get_tensor(output_index)[0][0]
 
-    # Convert probability to class
     prediction = 1 if probability >= 0.5 else 0
 
     st.subheader("Prediction Result")
